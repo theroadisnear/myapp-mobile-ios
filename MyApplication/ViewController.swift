d90f903b3459f8d58203e2d0098ef4dfa4ab4b5f
Rodner Raymundo <theroadisnear@Rodners-Mac.local>
2016-04-12 23:36:32 +0800

    Added authentication of user using login service

--- MyApplication/ViewController.swift
+++ MyApplication/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var avatarImage: UIImageView!
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    var loginService = LoginService()
     override func viewDidLoad() {
         super.viewDidLoad()
+        avatarImage.layer.borderWidth = 1.0
+        avatarImage.layer.masksToBounds = false
+        avatarImage.layer.borderColor = UIColor.whiteColor().CGColor
+        avatarImage.layer.cornerRadius = 70
+        avatarImage.clipsToBounds = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +30,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func login(sender: AnyObject) {
+        if usernameTextField.text == "" && passwordTextField.text == "" {
+            self.displayMessage("Username or password is empty")
+        } else {
+            loginService.authenticate(usernameTextField.text!, password: passwordTextField.text!, completionHandler: { responseObject in
+                if  responseObject!["error"] != nil {
+                    self.displayMessage(responseObject!["error"] as! String)
+                } else {
+                    self.displayMessage(responseObject!["message"] as! String)
+                }
+            })
+        }
+    }
+    
+    func  displayMessage(message: String){
+        let alertController = UIAlertController(title: "Message", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
 }
 
